d48a83162c3d9de7c2c03c26e0d97627e75c7060
narindra2 <narindrandimby2@gmail>
2024-04-24 09:29:32 +0300

    commit

--- app/Http/Controllers/VerifiedUserController.php
+++ app/Http/Controllers/VerifiedUserController.php
@@ -4,21 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Model\UserVerify;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 use App\Providers\PostsHelperServiceProvider;
 
 class VerifiedUserController extends Controller
 {
     public function index(Request $request)
     {
-
-        $verifiedUsers = UserVerify::with(['user' => function( $users){
+        Cookie::queue(Cookie::forget('app_feed_prev_page'));
+        Cookie::queue(Cookie::forget('app_prev_post'));
+        $verifiedUsers = UserVerify::with(['user' => function( $user){
                                     /** has post > MIN_NB_POST_TO_BE_VISIBLE */
-                                    $users->withCount('posts') ->having('posts_count', '>', UserVerify::MIN_NB_POST_TO_BE_VISIBLE);
+                                        $user->withCount('posts')->having('posts_count', '>', UserVerify::MIN_NB_POST_TO_BE_VISIBLE);
                                     }])
                                    ->where('status', 'verified')
                                    ->orderBy('id', 'DESC')
                                    ->paginate(10); // Pagination avec 10 éléments par page
-        PostsHelperServiceProvider::shouldDeletePaginationCookie($request);
+                                   dd();
         return view('verified_user', compact('verifiedUsers'));
     }
 }
