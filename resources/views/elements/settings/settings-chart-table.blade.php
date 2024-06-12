80d69d1fcf872ccbe4b168cceff82b28fed68627
narindra2 <narindrandimby2@gmail.com>
2024-06-12 22:34:18 +0300

    dashboar-with-date-filter-

--- resources/views/elements/settings/settings-chart-table.blade.php
+++ resources/views/elements/settings/settings-chart-table.blade.php
@@ -1,20 +1,12 @@
 
 <table class="table table-sm text-white">
-  <thead>
-    <tr>
-      <th scope="col">{{  time() }}</th>
-      <th scope="col">Type</th>
-      <th scope="col">Total</th>
-      <th scope="col">detail</th>
-    </tr>
-  </thead>
   <tbody>
     @foreach ( $datasets as $type )
         <tr>
         <th scope="row">Color</th>
         <td>{{ $type['label'] }}</td>
         <td>{{   collect($type['data'] )->sum() }} {{$currency_code}}</td>
-        <td>Voir detail</td>
+        <td>Detail</td>
         </tr>
     @endforeach
   </tbody>
